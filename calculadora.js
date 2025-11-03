12d0c901102fe3d46b0b4cae0e659fb854387e5b
giovanegiow <112131776+giovanegiow@users.noreply.github.com>
2025-11-03 15:56:08 -0300

    feat: Criação do branch e adição do código

---
+++ calculadora.js
@@ -0,0 +1,62 @@
+const { evaluate } = math;
+
+window.onload = function () {
+    document.getElementById("resultadoCalculadora").disabled = true;
+
+    document.getElementById("btnLimpar").addEventListener("click", () => document.getElementById("resultadoCalculadora").value = "");
+
+    document.querySelectorAll(".btnCalculadora").forEach(btn => {
+        btn.addEventListener("click", () => insereDigitoInput(btn));
+    })
+
+    document.getElementById("excluirUltimoDigito").addEventListener("click", () => limpaUltimoDigito());
+
+    document.getElementById("botaoCalcular").addEventListener("click", () => realizaCalculo());
+}
+
+function insereDigitoInput(botao){
+    if (document.getElementById("resultadoCalculadora").value == "Operação inválida!"){
+        document.getElementById("resultadoCalculadora").value = "";
+        document.getElementById("resultadoCalculadora").value += botao.innerText;
+    } else {
+        document.getElementById("resultadoCalculadora").value += botao.innerText;
+    }
+}
+
+function realizaCalculo() {
+    let calculoOperacao = trataCalculo(document.getElementById("resultadoCalculadora").value);
+
+    let resultado = trataResultado(calculoOperacao.toFixed(2));
+
+    adicionarOperacaoHistorico(document.getElementById("resultadoCalculadora").value, resultado);
+
+    document.getElementById("resultadoCalculadora").value = resultado;
+}
+
+function trataCalculo(valorOperacao) {
+    try {
+        if (valorOperacao.includes("x")) valorOperacao = valorOperacao.replaceAll("x", "*");
+
+        if (valorOperacao.includes(",")) valorOperacao = valorOperacao.replaceAll(",", ".");
+
+        return evaluate(valorOperacao);
+    } catch {
+        return "Operação inválida!";
+    }
+}
+
+function trataResultado(resultadoOperacao) {
+    if (resultadoOperacao.toString().includes(".")) resultadoOperacao = resultadoOperacao.toString().replace(".", ",");
+
+    return resultadoOperacao;
+}
+
+function limpaUltimoDigito(){
+    let valorAtual = document.getElementById("resultadoCalculadora").value;
+
+    document.getElementById("resultadoCalculadora").value = valorAtual.substring(0, valorAtual.length - 1);
+}
+
+function adicionarOperacaoHistorico(formula, resultado){
+    document.getElementById("operacoesRealizadas").innerHTML += `${formula} = ${resultado}<br>`;
+}
